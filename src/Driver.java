bc14616a95139af88aa454d790d08f9162caf232
cyztic <jrw795@lindenwood.edu>
2019-02-11 14:32:37 -0600

    added anpther scenerio

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,9 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Driver {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //MAP PRACTICE WITH STREAMS
         Map<Integer, String> map = new HashMap<Integer, String>();
         map.put(10, "Josh");
@@ -13,6 +11,8 @@ public class Driver {
         map.put(0, "Kari");
         map.put(89, "Robert");
         map.put(20000, "Zane");
+        map.put(2071, "Rick");
+        map.put(1, "Trevor");
 
         System.out.println("Sorted by key: ");
         map.entrySet().stream()
@@ -25,6 +25,27 @@ public class Driver {
         map.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.naturalOrder()))
                 .forEach(System.out::println);
-    }
 
+        System.out.println();
+
+        System.out.println("Print only greater than 15: ");
+        map.entrySet().stream()
+                .filter((x -> x.getKey() > 15))
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        System.out.println("Create new map of only even numbers");
+        //map to map
+        Map<Integer, String> y =
+                map.entrySet().stream()
+                        .filter((x -> x.getKey() % 2 == 0))
+                        .collect(Collectors.toMap(
+                                e -> (e.getKey()),
+                                e -> (e.getValue())
+                        ));
+
+        y.entrySet().stream()
+                .forEach(System.out::println);
+    }
 }
